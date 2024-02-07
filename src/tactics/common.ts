bbf7b42891ed194280ccf5f1de09cc22b108d30b
James Randall <james@accidentalfish.com>
2024-02-07 07:18:34 +0000

    Little helper for the AI flag

---
+++ src/tactics/common.ts
@@ -0,0 +1,14 @@
+import { AttitudeEnum, ShipInstance } from "../model/ShipInstance"
+
+export function aiFlag(player: ShipInstance) {
+  // The original game stores various bits of info in a single byte but then also uses the total value of the byte in
+  // various comparisons. To keep this code "modern" I've broken the flag out into properties (we're not exactly worrying
+  // about performance and memory here!) but the original tactics routines use this value for various comparisons.
+  // This function composes the original value from the properties
+  return (
+    (player.hasECM ? 1 : 0) |
+    ((player.aggressionLevel & 31) << 1) |
+    (player.attitude === AttitudeEnum.Hostile ? 64 : 0) |
+    (player.aiEnabled ? 128 : 0)
+  )
+}
--- src/tactics/manouver.ts
+++ src/tactics/manouver.ts
@@ -1,9 +1,16 @@
 import { ShipInstance } from "../model/ShipInstance"
 import { vec3 } from "gl-matrix"
 import { Game } from "../model/game"
+import { scannerRadialWorldRange } from "../constants"
+
+function isFarAway(ship:ShipInstance) {
+  return vec3.length(ship.position) > (scannerRadialWorldRange[0] / 2)
+}
 
 export function flyTowards(ship: ShipInstance, position: vec3) {}
 
 export function rollShipByNoticeableAmount(ship: ShipInstance) {}
 
-export function steerShip(ship: ShipInstance, game: Game) {}
+export function steerShip(ship: ShipInstance, game: Game) {
+  if ()
+}
